d194cf0b0777e450683e1920e340d77539b9007a
Márcio Martins <a.marcio.sm@gmail.com>
2021-12-18 07:38:06 -0300

    Alimentar Cliente e Endereco

--- src/main/java/com/gmail/amarciosm/controledepedidos/ControleDePedidosApplication.java
+++ src/main/java/com/gmail/amarciosm/controledepedidos/ControleDePedidosApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gmail.amarciosm.controledepedidos.domain.Categoria;
 import com.gmail.amarciosm.controledepedidos.domain.Cidade;
+import com.gmail.amarciosm.controledepedidos.domain.Cliente;
+import com.gmail.amarciosm.controledepedidos.domain.Endereco;
 import com.gmail.amarciosm.controledepedidos.domain.Estado;
 import com.gmail.amarciosm.controledepedidos.domain.Produto;
+import com.gmail.amarciosm.controledepedidos.enuns.TipoCliente;
 import com.gmail.amarciosm.controledepedidos.repositories.CategoriaRepository;
 import com.gmail.amarciosm.controledepedidos.repositories.CidadeRepository;
+import com.gmail.amarciosm.controledepedidos.repositories.ClienteRepository;
+import com.gmail.amarciosm.controledepedidos.repositories.EnderecoRepository;
 import com.gmail.amarciosm.controledepedidos.repositories.EstadoRepository;
 import com.gmail.amarciosm.controledepedidos.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class ControleDePedidosApplication implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ControleDePedidosApplication.class, args);
@@ -54,7 +65,8 @@ public class ControleDePedidosApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
+		*/
+		/*
 		Estado est1 = new Estado(null, "São Paulo");
 		Estado est2 = new Estado(null, "Minas Gerais");
 		
@@ -71,6 +83,18 @@ public class ControleDePedidosApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
+		*/
+		/*
+		Cliente cli1 = new Cliente(null, "Maria dos Anjos", "maria@gmail.com", "000.000.000-01", TipoCliente.PESSOA_FISICA.getCod());
+		cli1.getTelefones().addAll(Arrays.asList("3522-4582","8855-7281"));
+		
+		Endereco end1 = new Endereco(null, "Rua das Flores", "1", null, "Jardim", "50000-001", cli1, cid1);
+		Endereco end2 = new Endereco(null, "Av Javali", "2", null, "Selva", "70000-002", cli1, cid2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
+		
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(Arrays.asList(end1,end2));
 		*/
 	}
 
