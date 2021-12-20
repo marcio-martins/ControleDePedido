d31754872c8e4fddfe8bd6cc538dea7bb3be23c3
Márcio Martins <a.marcio.sm@gmail.com>
2021-12-20 20:51:12 -0300

    fix

--- src/main/java/com/gmail/amarciosm/controledepedidos/config/SecurityConfig.java
+++ src/main/java/com/gmail/amarciosm/controledepedidos/config/SecurityConfig.java
@@ -25,9 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
-			"/produtos/**",
-			"/categorias/**"
-		};
+		"/produtos/**",
+		"/categorias/**"
+	};
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
