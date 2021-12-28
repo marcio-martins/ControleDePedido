65d7371cf458703376d9cc874cd71f0f25415726
Márcio Martins <a.marcio.sm@gmail.com>
2021-12-28 10:53:48 -0300

    Personalizar excessao de validacao de campos

---
+++ src/main/java/com/gmail/amarciosm/controledepedidos/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.gmail.amarciosm.controledepedidos.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.validation.FieldError;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String field, String message) {
+		errors.add(new FieldMessage(field, message));
+	}
+	
+}
